f730b98334683aaf797b13f7bd9adaef952c31b6
vincenzodivita <divitavincenzo@icloud.com>
2023-08-04 20:20:03 +0200

    Add empty project and import Scanner

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Import Scanner
+        Scanner scanner = new Scanner(System.in);
+
+        // Let's code!
+    }
+}
